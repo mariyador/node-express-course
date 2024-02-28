3444325604babd82086a06139a566295b16f27ff
Mariya <mariya@MacBook-Air-Mariya.local>
2024-02-27 20:08:04 -0500

    Guessing game in prompter.js file.

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -21,16 +21,17 @@ const getBody = (req, callback) => {
 };
 
 // here, you could declare one or more variables to store what comes back from the form.
-let item = "Enter something below.";
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+let message = "Guess a number from 1 to 100:";
 
 // here, you can change the form below to modify the input fields and what is displayed.
 // This is just ordinary html with string interpolation.
 const form = () => {
   return `
   <body>
-  <p>${item}</p>
+  <p>${message}</p>
   <form method="POST">
-  <input name="item"></input>
+  <input type="number" name="guess" min="1" max="100" required></input>
   <button type="submit">Submit</button>
   </form>
   </body>
@@ -40,25 +41,36 @@ const form = () => {
 const server = http.createServer((req, res) => {
   console.log("req.method is ", req.method);
   console.log("req.url is ", req.url);
+
   if (req.method === "POST") {
     getBody(req, (body) => {
       console.log("The body of the post is ", body);
-      // here, you can add your own logic
-      if (body["item"]) {
-        item = body["item"];
-      } else {
-        item = "Nothing was entered.";
+
+      if (body["guess"]) {
+        const userGuess = parseInt(body["guess"]);
+
+        if (!isNaN(userGuess)) {
+          if (userGuess === secretNumber) {
+            message = "Congratulations! You guessed the correct number.";
+            secretNumber = Math.floor(Math.random() * 100) + 1;
+          } else if (userGuess < secretNumber) {
+            message = "Too low. Try again!";
+          } else if (userGuess > secretNumber) {
+            message = "Too high. Try again!";
+          } 
+        } else {
+          message = "Invalid input. Please enter a number between 1 and 100.";
+        }
       }
-      // Your code changes would end here
-      res.writeHead(303, {
-        Location: "/",
-      });
-      res.end();
+      // After handling the POST request, display the result message
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(form());
     });
   } else {
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(form());
   }
 });
-
+    
 server.listen(3000);
 console.log("The server is listening on port 3000.");
