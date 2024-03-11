fc66808c537c8f6fb4265e122727e7fee78a82fb
Mariya <mariya@MacBook-Air-Mariya.local>
2024-03-10 21:51:38 -0400

    set up Express app, create 'public' folder with 'index.html', implement JSON API endpoint, and test functionality.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,19 @@
-console.log('Express Tutorial')
+// console.log('Express Tutorial')
+
+const express = require('express');
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get('/api/v1/test', (req, res) => {
+    res.json({ message: "It worked!" });
+  });
+  
+app.use((req, res, next) => {
+    res.status(404).send("404 Error: Page Not Found");
+  });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
